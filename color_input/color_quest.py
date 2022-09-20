a06cb3dc0489a0790ca5bde6451886a73dbeefaf
Carl Walker <cwalker159@users.noreply.github.com>
2022-09-20 14:33:56 +0000

    In class labs for Python

---
+++ color_input/color_quest.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# input asking for your name
+name = input("What is your name?")
+
+# input asking for the day of the week
+day = input("what day is it?")
+
+# print my statement
+print("Hello, " + name + "! Happy " + day + "!")
+
+
