62fd631be5bd946a5245191a2d06149bb8d19e0c
cwalker159 <cwalker159@gmail.com>
2022-09-27 17:30:54 +0000

    Working with Pandas part 2

--- pandas2/pandabear02.py
+++ pandas2/pandabear02.py
@@ -3,23 +3,21 @@
 import pandas as pd
 
 def main():
-    # create a dataframe ciscocsv
     ciscocsv = pd.read_csv("ciscodata.csv")
-    # create a dataframe ciscojson:
     ciscojson = pd.read_json("ciscodata2.json")
-    
+
     # display first 5 entries of the ciscocsv dataframe
     print(ciscocsv.head())
 
     # display first 5 entries of the ciscojson dataframe            
     print(ciscojson.head())
-    
+
     ciscodf = pd.concat([ciscocsv, ciscojson])
     # uncomment the line below to "fix" the index issue
     # ciscodf = pd.concat([ciscocsv, ciscojson], ignore_index=True, sort=False)
-    
+
     print(ciscodf)
-    
+
 if __name__ == "__main__":
     main()
 
