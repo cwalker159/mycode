7720877c1eb043968f0580ac53074ff688e77003
cwalker159 <cwalker159@gmail.com>
2022-09-27 20:13:09 +0000

    csv and pandas practice

---
+++ superheroes/superpowers.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import classinfo
+
+# parts 1 and 2
+name= classinfo["all"][2]["name"]
+power= classinfo["all"][2]["super power"]
+
+print(name, "has the power of", power)
+
+# part 3
+for x in classinfo["all"]:
+    name= x["name"]
+    skill= x["skill level"]
+    power= x["super power"]
+    animal= x["spirit animal"]
+
+    # Mario, a <wondrous> <alpaca> of a programmer, possesses a <structure weakening> factor for moonlighting as a superhero!
+    print(f"{name}, a {skill} {animal} of a programmer, possesses a {power} factor for moonlighting as a superhero!")
